72324c58c404bf2e86656b68dd226b7b34f311bc
Hubai Rajmund Szilveszter <hubairajmund0507@gmail.com>
2023-08-18 18:55:11 +0200

    Update color

--- Assets/Script/ColorScript.cs
+++ Assets/Script/ColorScript.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 
 public class ColorScript : MonoBehaviour
 {
-    public Color colorOfTheSprite;
-    private GameObject FinalDrink;
-    private DrinkScript finalDrink;
-    private DrinkStorage Storage;
     Renderer FinalDrinkRenderer;
     Renderer MixDrinkRenderer;
+
+    Vector4 colorOfFinalDrink;
+    Vector4 colorOfMixDrink;
     // Start is called before the first frame update
     void Start()
     {
         MixDrinkRenderer = gameObject.GetComponent<Renderer>();
+        
         MixDrinkRenderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         FinalDrinkRenderer = GameObject.FindWithTag("Drink").GetComponent<Renderer>();
         FinalDrinkRenderer.material.color = Color.white;
 
-        Vector4 colorOfTheFinalDrink = FinalDrinkRenderer.material.color;
-        Vector4 colorOfTheMixDrink = MixDrinkRenderer.material.color;
+        Vector4 colorOfFinalDrink = FinalDrinkRenderer.material.color;
+        Vector4 colorOfMixDrink = MixDrinkRenderer.material.color;
         
     }
 
     void OnMouseDown()
     {
+
         Debug.Log("Sikeres");
 
     }
